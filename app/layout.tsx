02ebc62e57bc7aee1ffefb4dc3e8f53dac63a030
Henry Catalini Smith <henry@catalinismith.se>
2025-02-07 08:46:57 +0000

    update starter to reflect new patterns

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Footer } from "@arbetsmarknad/components/Footer";
+import { HeaderMenu } from "@arbetsmarknad/components/HeaderMenu";
+import { Page } from "@arbetsmarknad/components/Page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Page>
+          <HeaderMenu
+            href="https://arbetsmarknad.github.io"
+            text="arbetsmarknad.github.io"
+          />
+          {children}
+          <Footer
+            sourceCode={["arbetsmarknad/next", "arbetsmarknad/components"]}
+          />
+        </Page>
       </body>
     </html>
   );
